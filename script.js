45479eb8dc111cc7574c6a5d9ef8c615316b7d11
belgacemSalah <salahmiz@gmail.com>
2025-01-22 18:14:08 +0100

    oui
    site

---
+++ script.js
@@ -0,0 +1,13 @@
+function afficherMessage() {
+    alert("C'est moi l'ingèrnieur Ahmed Salah !");
+}
+
+function affichedate() {
+    alert("La date 22/01/25 !");
+    
+}
+
+function afficheage() {
+    alert("J'ai crée mon Site Web à l'age 11 ans !");
+    
+}
